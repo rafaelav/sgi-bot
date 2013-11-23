414ef949194066ff81480c3ddc923891cf10a670
rafaelav <rafaela.voiculescu@gmail.com>
2013-11-23 11:48:01 +0100

    made random wait script, made script to run once a day

--- src/start_processes.sh
+++ src/start_processes.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python unfollow_script.py
+python random_wait_script.py && python save_data_script.py && python follow_script.py && (python unfollow_script.py & python favorite_script.py)
 
