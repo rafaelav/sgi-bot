d062f6e3dc7269a2ff15008054bed77d4d00559a
Constantin Teodor Gherghescu <gherghescu.teo@gmail.com>
2013-11-25 14:55:02 +0100

    Added tweeting and retweeting to the startup script

--- src/start_processes.sh
+++ src/start_processes.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python random_wait_script.py && python save_data_script.py && python follow_script.py && (python unfollow_script.py & python favorite_script.py)
+python random_wait_script.py && python save_data_script.py && python follow_script.py && (python unfollow_script.py & python favorite_script.py & python tweet_script.py & python retweet_script.py)
 
